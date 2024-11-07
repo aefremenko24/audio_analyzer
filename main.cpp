bf1287e490ba2afb0a5951a5e88a2e28efac4d28
Arthur Efremenko <aefremenko24@gmail.com>
2024-11-06 23:20:41 -0500

    add inline comments to variables defined outside of functions

--- main.cpp
+++ main.cpp
@@ -9,25 +9,46 @@
 
 using namespace std;
 
-#define SAMPLE_RATE 44100.0
-#define FRAMES_PER_BUFFER 2048
+/*
+NOTE: Coordinates used in this project assume (0, 0) is the top left corner of the terminal.
+ Increasing y moves the cursor down; increasing x moves the cursor to the right.
+ */
 
+// Rate at which the audio is sampled, in Hz
+#define SAMPLE_RATE 44100.0
+
+// Number of data points collected in a single buffer
+#define FRAMES_PER_BUFFER 1024
+
+// Frequency display range minimum and maximum (normal human hearing range here)
 #define SPECTRO_FREQ_START 20
 #define SPECTRO_FREQ_END 20000
 
+// Width of the console display window in number of characters
 #define WIN_WIDTH 100
+
+// Number of lines to be skipped between different view windows
 #define MARGIN 2
 
+// x- and y-coordinates of the top left corner of the volume view window
 #define VOL_INIT_X 0
 #define VOL_INIT_Y 0
 
+// The height of the frequency view window in number of lines
 #define FREQ_WIN_HEIGHT 20
+
+// y-coordinate of the top left corner of the frequency view window
 #define FREQ_INIT_X 0
 
+// Data structures representing the volume and frequency view windows
 WINDOW* VOL_WIN;
 WINDOW* FREQ_WIN;
+
+// Map representing the recent maximum measurement of the amplitude at each frequency.
+// Each value in the map will be decremented over time until new max is set.
 std::unordered_map<int, double> current_max;
 
+// Number of input channels for the input source the program is working with
 int num_channels;
 
 void init_vol_win(int num_channels = 1) {
